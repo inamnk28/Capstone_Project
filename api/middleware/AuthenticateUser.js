1b6f32b6de2de678797f85363aa1ac3c610782f2
inamnk28 <inamnkabi1@gmail>
2023-09-13 15:42:29 +0200

    api

--- api/middleware/AuthenticateUser.js
+++ api/middleware/AuthenticateUser.js
@@ -13,21 +13,21 @@ function createToken(user) {
         expiresIn: '1h'
     })
 }
-function verifyToken(req,res,next){
-    try{
-        const token = req.headers["authorization"]
+// function verifyToken(req,res,next){
+//     try{
+//         const token = req.headers["authorization"]
         
-        next()
+//         next()
 
-    }
-    catch(e){
-        res.json({
-            status: res.statusCode,
-            msg: e.message
-        })
-    }
+//     }
+//     catch(e){
+//         res.json({
+//             status: res.statusCode,
+//             msg: e.message
+//         })
+//     }
  
-} 
+// } 
 // function verifyAToken(req, res, next) {
 //     const token = req.header("authorization");
 
