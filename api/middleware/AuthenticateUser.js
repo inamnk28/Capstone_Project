fe76b15e863df233aba80e6ded2bb308c0c3a602
inamnk28 <inamnkabi1@gmail>
2023-09-13 15:47:14 +0200

    cap

--- api/middleware/AuthenticateUser.js
+++ api/middleware/AuthenticateUser.js
@@ -10,21 +10,6 @@ function createToken(user) {
         expiresIn: '1h'
     })
 }
-function verifyAToken(req, res, next){
-   try{
-        // Retrieve token from req.headers
-        console.log("Get token from req.headers['authorization']");
-        const token = req.headers["authorization"]
-        console.log(token);
-        next()
-   }catch(e){
-        res.json({
-            status: res.statusCode,
-            msg: e.message
-        })
-   }
-}
 module.exports = {
  createToken,
- verifyAToken
 }
