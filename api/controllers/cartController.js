1fd3ddcf76c3794d21fd7cf18523bd50a569767a
inamnk28 <inamnkabi1@gmail>
2023-09-13 13:57:13 +0200

    api

--- api/controllers/cartController.js
+++ api/controllers/cartController.js
@@ -1,38 +1,83 @@
 const { insertCartItem, getCartItemsByUserId, deleteCartItem } = require("../models/cartModel.js");
 
 // Add item to cart
+// const addToCart = (req, res) => {
+//     const data = req.body;
+//     insertCartItem(data, (err, results) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.json(results);
+//         }
+//     });
+// };
 const addToCart = (req, res) => {
     const data = req.body;
+    // Ensure that the data includes userId, product_id, and quantity
+    if (!data.userId || !data.product_id || !data.quantity) {
+        return res.status(400).json({ error: "Invalid request data" });
+    }
+
     insertCartItem(data, (err, results) => {
         if (err) {
-            res.send(err);
-        } else {
-            res.json(results);
+            console.error(err);
+            return res.status(500).json({ error: "Failed to add item to cart" });
         }
+        res.json({ message: "Item added to cart successfully", results });
     });
 };
 
 // View user's cart
+// const viewCart = (req, res) => {
+//     const user_id = req.params.user_id;
+//     getCartItemsByUserId(user_id, (err, results) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.json(results);
+//         }
+//     });
+// };
 const viewCart = (req, res) => {
     const user_id = req.params.user_id;
-    getCartItemsByUserId(user_id, (err, results) => {
+    // Ensure that user_id is valid (e.g., numeric or whatever format you use)
+    if (!user_id) {
+        return res.status(400).json({ error: "Invalid user ID" });
+    }
+
+    getCartItemsByProductId(user_id, (err, results) => {
         if (err) {
-            res.send(err);
-        } else {
-            res.json(results);
+            console.error(err);
+            return res.status(500).json({ error: "Failed to retrieve cart items" });
         }
+        res.json({ cartItems: results });
     });
 };
 
 // Delete item from cart
+// const removeFromCart = (req, res) => {
+//     const cart_id = req.params.cart_id;
+//     deleteCartItem(cart_id, (err, results) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.json(results);
+//         }
+//     });
+// };
 const removeFromCart = (req, res) => {
     const cart_id = req.params.cart_id;
+    // Ensure that cart_id is valid (e.g., numeric or whatever format you use)
+    if (!cart_id) {
+        return res.status(400).json({ error: "Invalid cart ID" });
+    }
+
     deleteCartItem(cart_id, (err, results) => {
         if (err) {
-            res.send(err);
-        } else {
-            res.json(results);
+            console.error(err);
+            return res.status(500).json({ error: "Failed to delete item from cart" });
         }
+        res.json({ message: "Item removed from cart successfully", results });
     });
 };
 
